6989bdd3b86fb68468ff898c3c7216b152515902
Hanna <kazkovaanna76@mail.ru>
2022-05-26 14:11:03 +0300

    task1
    start task

---
+++ script.js
@@ -0,0 +1,11 @@
+//1
+let name = prompt ('Ваше имя');
+let age = prompt ('Ваш возраст', 100);
+let city =  prompt ('Ваш город проживания');
+let company =  prompt ('Ваша компания');
+let phone = prompt ('Ваш телефон');
+let email = prompt ('Ваш адрес электронной поты');
+
+
+
+
