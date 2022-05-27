b7774d9a063f0b3787d0b602fd2a36b07d83fa8c
Hanna <kazkovaanna76@mail.ru>
2022-05-27 13:21:01 +0300

    task
    doing exercises

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 //1
-let name = prompt ('Ваше имя');
-let age = prompt ('Ваш возраст', 100);
+
+/*let user = prompt ('Ваше имя');
+let age = prompt ('Ваш возраст');
 let city =  prompt ('Ваш город проживания');
 let company =  prompt ('Ваша компания');
 let phone = prompt ('Ваш телефон');
 let email = prompt ('Ваш адрес электронной поты');
 
+alert (`Меня зовут ${user}.` + '\n ' + `Мне`  +  `${age}`  +  'лет.' + '\n ' + `Я проживаю в городе ${city}.` + '\n ' + `Я работаю в ${company}.` + '\n ' + `Мои контактные данные: ${phone}  ${email}.` );
 
+//2
 
+let year=(2020 - age);
+
+console.log(year);
+console.log(user + 'родился в ' + year + 'году' );*/
+
+//3
+
+//let str = "121212";
+let str = "333222";
+
+if ( (Number(str[0]) + Number(str[1]) + Number(str[2])) ==  (Number(str[3]) + Number(str[4]) + Number(str[5])) ) {
+
+console.log("да");
+} 
+else {
+    console.log("нет");
+}
+
+//4
+
+//let a = (1);
+//let a = (0);
+let a = (-3);
+
+if(a > null){
+    console.log("Верно");
+ }
+ else{
+    console.log("Неверно");
+ }
 
